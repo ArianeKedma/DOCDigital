e32fa7b5f80d2dafb721b40e3745b1b50beceba6
LAVÍNIA DOMINGOS <lavinia_domingos@estudante.sesisenai.org.br>
2025-08-04 16:37:04 -0300

    Atualizações para fazer

---
+++ src/app/administrador/page.js
@@ -0,0 +1,10 @@
+export default function Admin() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-center">
+      <div>
+        <h1 className="text-3xl font-bold">Área do Administrador</h1>
+        <p className="mt-2">Conteúdo restrito para administradores.</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/representante/page.js
@@ -0,0 +1,17 @@
+// /app/representantes/primeiraSecao/page.js
+
+export default function Representante() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center text-center gap-8">
+      <div>
+        <h1 className="text-3xl font-bold">Área do Representante</h1>
+        <p className="mt-2">Bem-vindo à Área do Representante.</p>
+      </div>
+
+      {/* Aqui você pode adicionar mais conteúdo ou seções específicas para a Primeira Sessão */}
+      <div className="mt-6">
+        <p>Essa é uma área de introdução para o Representante.</p>
+      </div>
+    </div>
+  );
+}
